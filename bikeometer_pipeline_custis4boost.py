2c46ef22e4a557138a13d71845d48b77b969f3be
GeorgeTSilva <georges083@gmail.com>
2017-04-25 22:08:31 -0400

    Updated bikeometer_pipeline_custis4boost.py

--- bikeometer_pipeline_custis4boost.py
+++ bikeometer_pipeline_custis4boost.py
@@ -2,6 +2,8 @@ import numpy as np
 
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MaxAbsScaler
 
 # NOTE: Make sure that the class is labeled 'class' in the data file
 tpot_data = np.recfromcsv('PATH/TO/DATA/FILE', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,7 +11,10 @@ features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpo
 training_features, testing_features, training_classes, testing_classes = \
     train_test_split(features, tpot_data['class'], random_state=42)
 
-exported_pipeline = GradientBoostingRegressor(alpha=0.85, learning_rate=0.1, loss="ls", max_depth=6, max_features=0.75, min_samples_leaf=6, min_samples_split=16, subsample=1.0)
+exported_pipeline = make_pipeline(
+    MaxAbsScaler(),
+    GradientBoostingRegressor(alpha=0.9, loss="ls", max_depth=5, max_features=0.4, min_samples_leaf=15, min_samples_split=7, subsample=0.8500000000000001)
+)
 
 exported_pipeline.fit(training_features, training_classes)
 results = exported_pipeline.predict(testing_features)
